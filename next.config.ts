7117c94f630858ed3ac1f2e0ccdad8d92e34e69b
NurdauletAskar <49223685+NurdauletMyrza@users.noreply.github.com>
2025-03-31 15:32:55 +0500

    fix: middleware.ts for vercel bug

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    middlewarePrefetch: "flexible", // Улучшает работу Middleware
+  },
 };
 
 export default nextConfig;
